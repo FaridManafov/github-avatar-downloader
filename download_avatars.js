82810a5e75bf20a0cbc864157220fa4133469472
Farid <faridbardium@gmail.com>
2018-07-23 14:37:04 -0400

    added download and save avatars functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,29 +1,44 @@
 var request = require('request');
 var secrets = require('./secrets')
+require("dotenv").config();
+
+var repositoryOwner = process.argv.slice(2)[0];
+var repositoryName = process.argv.slice(2)[1];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, callback) {
-    var options = {
-        url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-
-        headers : { 
-            'User-Agent' : 'request',
-            'Authorization' : 'token ' + secrets.GITHUB_TOKEN
-        }
-        
+var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers : { 
+        'User-Agent' : 'FaridManafov',
+        'Authorization': process.env.GITHUB_TOKEN
     }
+}
 
-    request(options, function(err, res, body){
-        callback(err, JSON.parse(body));
-    });
+var getAvatars = function(options, save) {
+  request.get(options, function(error, response, body) {
+    if (error) {
+      console.log('ERROR: ' + response.statusCode);
+      throw err;
+    } else {
+      var rawData = JSON.parse(body);
+      rawData.forEach(function(element) {
+        saveAvatar(element.avatar_url, element.login);
+      })
+    }
+  });
+}
 
+var saveAvatar = function (url, name) {
+    request.get(url)
+        .on('response', function () {
+            process.stdout.write('Downloading avatar for user ' + name + ' to the avatars file \n');
+        })
+        .pipe(fs.createWriteStream('./avatars/' + name + '.png'))
 };
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    // console.log("Result:", result);
-    result.forEach(function(user){
-        console.log(user.avatar_url)
-    });
-  });
+process.argv.slice(2).length === 2
+  ? getAvatars(options, saveAvatar)
+  : console.log(
+      "ERROR: Arguments must be in the form of 'repo_owner' 'repo_name'. Ex: node avatar_get.js facebook react"
+    );
